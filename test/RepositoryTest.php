e499e9056842a791c821c68f767563bd185a2675
Luis Rovirosa <luisrovirosa@gmail.com>
2015-04-04 13:34:48 +0200

    TransactionFactoryTest refactoring

--- test/RepositoryTest.php
+++ test/RepositoryTest.php
@@ -5,19 +5,34 @@ namespace KataBank\Test;
 
 use KataBank\Repository;
 use Prophecy\PhpUnit\ProphecyTestCase;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class RepositoryTest extends ProphecyTestCase
 {
+    /**
+     * @var Repository
+     */
+    private $repository;
+
+    /**
+     * @var ObjectProphecy
+     */
+    private $transactionFactoryProphecy;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->transactionFactoryProphecy = $this->prophesize('KataBank\TransactionFactory');
+        $this->repository = new Repository($this->transactionFactoryProphecy->reveal());
+    }
+
     /**
      * @test
      */
     public function deposit_creates_a_transaction()
     {
-        $transactionFactoryProphecy = $this->prophesize('KataBank\TransactionFactory');
-        $repository = new Repository($transactionFactoryProphecy->reveal());
+        $this->repository->deposit(100);
 
-        $repository->deposit(100);
-
-        $transactionFactoryProphecy->makeDeposit(100)->shouldBeCalled();
+        $this->transactionFactoryProphecy->makeDeposit(100)->shouldBeCalled();
     }
 }
